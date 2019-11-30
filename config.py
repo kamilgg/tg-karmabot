63f7184179ef641791faac4d5d73b4ee5cc3fcad
Kamil' Gatin <kamilgg26@yandex.ru>
2019-11-30 15:05:39 +0300

    variables renamed to pep8

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 token = ""  # bot token
-botName = ""  # bot username
-chatId = -1001182112156  # id of chat where bot should work
+bot_name = ""  # bot username
+chat_id = -1001182112156  # id of chat where bot should work
 
 # DB settings
 database = "bnf747l6otb4n"
@@ -10,7 +10,7 @@ password = "0c2gbb1983776f19837ddaf1218736661b30dc1b8e198739bdde0dd7a7b04ad"
 
 admins = [183654961, 0, 0]  # users ids that can use /ban and /unban commands
 
-helpText = """
+help_text = """
 To *add karma*, reply with the phrase "Thank you" to the message of the person who helped you.
 \nTo *reduce karma*, reply with the phrase "Minus" to the message of who you want to reduce karma.
 
@@ -20,14 +20,14 @@ To *add karma*, reply with the phrase "Thank you" to the message of the person w
 """
 
 # text for new users
-welcomeText = """
+welcome_text = """
 Hey, @%s!
 \nWe are glad to see you in the chat.
 \nTell me something about yourself.
 """
 
 # text for new users without username
-welcomeUser = """
+welcome_user = """
 Hey, %s!
 \nWe are glad to see you in the chat.
 \nI noticed that you don't have a username :( Please set it, so that I can change your karma. Thank you!
@@ -35,47 +35,47 @@ Hey, %s!
 """
 
 # text for chat where bot don't work
-dontWork = "Sorry, but I don't work in this chat."
+chat_error = "Sorry, but I don't work in this chat."
 
 # karma text for user without username
-notUsername = "Unfortunately, the user you thanked doesn't have a username, so I can't change his karma :("
+username_error = "Unfortunately, the user you thanked doesn't have a username, so I can't change his karma :("
 
 # text for message without reply
-shouldReply = "@%s, you must respond to a message that helped you!"
-shouldReply2 = "@%s, you must respond to a message you didn't like!"
+reply_to_like = "@%s, you must reply to a message that helped you!"
+reply_to_unlike = "@%s, you must reply to a message you didn't like!"
 
 # text with thanks to bot
-thxToBot = "@%s, always happy to help!"
+like_to_bot = "@%s, always happy to help!"
 
 # text with reduce karma to bot
-botMinus = "@%s, I don't have a karma!"
+unlike_to_bot = "@%s, I don't have a karma!"
 
 # text with karma to urself
 masturbate = "@%s, hey, you can't add karma to yourself!"
 unmasturbate = "@%s, why don't you love yourself? :("
 
 # text to karma add
-karmaPlus = """
+like_message = """
 Added karma to @%s
 \n-------------------
 @%s has %d karma
 """
 
 # text to reduce karma
-karmaMinus = """
+unlike_message = """
 Reduces karma to @%s
 \n-------------------
 @%s has %d karma
 """
 
 # text for a ban for a little karma
-banForKarma = "@%s has -100 karma. He is banned."
+ban = "@%s has -100 karma. He is banned."
 
 # text for ban if user doesn't exist in DB
-cantBan = "There is no such user in my database, you can ban it only manually."
+cant_ban = "There is no such user in my database, you can ban it only manually."
 
 # text for unban if user doesn't exist in DB
-cantUnban = "There is no such user in my database, you can unban it only manually."
+cant_unban = "There is no such user in my database, you can unban it only manually."
 
 # text for ban
 banned = "Banned"
--- web.py
+++ web.py
@@ -9,10 +9,10 @@ from flask import Flask
 
 sys.path.append('./karmabot')
 
-from config import token, botName, chatId, admins, database, host, user, password
-from config import helpText, welcomeText, welcomeUser, dontWork, notUsername, shouldReply, thxToBot, botMinus, \
-    masturbate, unmasturbate, karmaPlus, karmaMinus, banForKarma, cantBan, \
-    cantUnban, banned, unbanned
+from config import token, bot_name, chat_id, admins, database, host, user, password
+from config import help_text, welcome_text, welcome_user, chat_error, username_error, reply_to_like, like_to_bot, unlike_to_bot, \
+    masturbate, unmasturbate, like_message, unlike_message, ban, cant_ban, \
+    cant_unban, banned, unbanned
 from db_control import DB, UsersModel, KarmaModel
 
 # connect to DB
@@ -26,128 +26,128 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['new_chat_members'])
 def welcome(w):
-    if w.chat.id == chatId:
+    if w.chat.id == chat_id:
         if w.new_chat_member.username is None:
             username = str(w.new_chat_member.first_name) + " " + str(w.new_chat_member.last_name)
-            bot.send_message(w.chat.id, text=welcomeUser % username)
+            bot.send_message(w.chat.id, text=welcome_user % username)
         else:
             username = str(w.new_chat_member.username)
             id = int(w.new_chat_member.id)
             users_model.new_user(id, username)
-            bot.send_message(w.chat.id, text=welcomeText % username)
+            bot.send_message(w.chat.id, text=welcome_text % username)
     else:
-        bot.send_message(w.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(w.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(commands=['help'])
 def help(h):
-    if h.chat.id == chatId:
-        bot.send_message(h.chat.id, parse_mode='Markdown', text=helpText)
+    if h.chat.id == chat_id:
+        bot.send_message(h.chat.id, parse_mode='Markdown', text=help_text)
     else:
-        bot.send_message(h.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(h.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(regexp="Thank you")
 def karma_plus(kp):
-    if kp.chat.id == chatId:
+    if kp.chat.id == chat_id:
         username = kp.from_user.username
         if kp.reply_to_message is None:
-            bot.send_message(kp.chat.id, parse_mode='Markdown', text=shouldReply % username)
+            bot.send_message(kp.chat.id, parse_mode='Markdown', text=reply_to_like % username)
         else:
             if (kp.reply_to_message.from_user.username is None) or ():
-                bot.send_message(kp.chat.id, text=notUsername)
+                bot.send_message(kp.chat.id, text=username_error)
             else:
                 to_user = kp.reply_to_message.from_user.username
                 user_id = kp.reply_to_message.from_user.id
-                if to_user == botName:
-                    bot.send_message(kp.chat.id, text=thxToBot % username)
+                if to_user == bot_name:
+                    bot.send_message(kp.chat.id, text=like_to_bot % username)
                 elif to_user == username:
                     bot.send_message(kp.chat.id, text=masturbate % username)
                 else:
                     karma_model.karma_plus(user_id, to_user)
                     karma = karma_model.current_karma(user_id)
-                    bot.send_message(kp.chat.id, text=karmaPlus % (to_user, to_user, karma))
+                    bot.send_message(kp.chat.id, text=like_message % (to_user, to_user, karma))
     else:
-        bot.send_message(kp.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(kp.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(regexp="Minus")
 def karma_minus(km):
-    if km.chat.id == chatId:
+    if km.chat.id == chat_id:
         username = km.from_user.username
         if km.reply_to_message is None:
-            bot.send_message(km.chat.id, parse_mode='Markdown', text=shouldReply % username)
+            bot.send_message(km.chat.id, parse_mode='Markdown', text=reply_to_like % username)
         else:
             if km.reply_to_message.from_user.username is None:
-                bot.send_message(km.chat.id, text=notUsername)
+                bot.send_message(km.chat.id, text=username_error)
             else:
                 to_user = km.reply_to_message.from_user.username
                 user_id = km.reply_to_message.from_user.id
-                if to_user == botName:
-                    bot.send_message(km.chat.id, text=botMinus % username)
+                if to_user == bot_name:
+                    bot.send_message(km.chat.id, text=unlike_to_bot % username)
                 elif to_user == username:
                     bot.send_message(km.chat.id, text=unmasturbate % username)
                 else:
                     ans = karma_model.karma_minus(user_id, to_user)
                     karma = karma_model.current_karma(user_id)
                     if ans == 1:
-                        bot.send_message(km.chat.id, text=karmaMinus % (to_user, to_user, karma))
+                        bot.send_message(km.chat.id, text=unlike_message % (to_user, to_user, karma))
                     elif ans == 2:
                         bot.restrict_chat_member(km.chat.id, user_id, can_send_messages=False,
                                                  can_send_media_messages=False, can_send_other_messages=False,
                                                  can_add_web_page_previews=False)
-                        bot.send_message(km.chat.id, text=banForKarma)
+                        bot.send_message(km.chat.id, text=ban)
     else:
-        bot.send_message(km.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(km.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(commands=['top20'])
 def top20(t20):
-    if t20.chat.id == chatId:
+    if t20.chat.id == chat_id:
         bot.send_message(t20.chat.id, parse_mode='Markdown', text=karma_model.top20())
     else:
-        bot.send_message(t20.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(t20.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(commands=['untop20'])
 def untop20(ut20):
-    if ut20.chat.id == chatId:
+    if ut20.chat.id == chat_id:
         bot.send_message(ut20.chat.id, parse_mode='Markdown', text=karma_model.untop20())
     else:
-        bot.send_message(ut20.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(ut20.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(commands=['ban'])
 def ban(b):
-    if b.chat.id == chatId:
+    if b.chat.id == chat_id:
         if b.from_user.id in admins:
             username = str(b.text)[6:]
             user_id = int(users_model.id_of_user(username))
             if user_id == 0:
-                bot.send_message(b.chat.id, text=cantBan)
+                bot.send_message(b.chat.id, text=cant_ban)
             else:
                 bot.restrict_chat_member(b.chat.id, user_id, can_send_messages=False, can_send_media_messages=False,
                                          can_send_other_messages=False, can_add_web_page_previews=False)
                 bot.send_message(b.chat.id, text=banned)
     else:
-        bot.send_message(b.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(b.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 @bot.message_handler(commands=['unban'])
 def unban(ub):
-    if ub.chat.id == chatId:
+    if ub.chat.id == chat_id:
         if ub.from_user.id in admins:
             username = str(ub.text)[8:]
             user_id = int(users_model.id_of_user(username))
             if user_id == 0:
                 bot.send_message(ub.chat.id,
-                                 text=cantUnban)
+                                 text=cant_unban)
             else:
                 bot.restrict_chat_member(ub.chat.id, user_id, can_send_messages=True, can_send_media_messages=True,
                                          can_send_other_messages=True, can_add_web_page_previews=True)
                 bot.send_message(ub.chat.id, text=unbanned)
     else:
-        bot.send_message(ub.chat.id, parse_mode='Markdown', text=dontWork)
+        bot.send_message(ub.chat.id, parse_mode='Markdown', text=chat_error)
 
 
 # webhook
@@ -176,6 +176,6 @@ def getMessage():
 
 bot.remove_webhook()
 sleep(1)
-bot.set_webhook(url="https://karmabot.herokuapp.com/%s/" % token)
+bot.set_webhook(url="https://yourwebhook.com/%s/" % token)
 
 server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)), debug=True)
